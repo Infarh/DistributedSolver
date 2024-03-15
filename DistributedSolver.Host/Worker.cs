7cad547a5e383c037553da5df10ba665d05cc86f
Павел <shmachilin@gmail.com>
2024-03-15 14:03:44 +0300

    Добавьте файлы проекта.

---
+++ DistributedSolver.Host/Worker.cs
@@ -0,0 +1,17 @@
+namespace DistributedSolver.Host;
+
+public class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken StoppingToken)
+    {
+        while (!StoppingToken.IsCancellationRequested)
+        {
+            if (logger.IsEnabled(LogLevel.Information))
+            {
+                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+
+            await Task.Delay(1000, StoppingToken).ConfigureAwait(false);
+        }
+    }
+}
